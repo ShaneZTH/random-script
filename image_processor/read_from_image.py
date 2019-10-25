3a369310b2de8a3aec125398ed9ba6a93b81ca67
Taohan Zhu <taz221@lehigh.edu>
2019-10-25 02:58:26 -0400

    detect_text API implemented

--- image_processor/read_from_image.py
+++ image_processor/read_from_image.py
@@ -8,20 +8,22 @@ def detect_labels_local_file(photo):
    
     with open(photo, 'rb') as image:
         # print(image.read())
-        response = client.detect_labels(Image={'Bytes': image.read()})
+        response = client.detect_text(Image={'Bytes': image.read()})
         
-    print('Detected labels in ' + photo)    
-    for label in response['Labels']:
-        print (label['Name'] + ' : ' + str(label['Confidence']))
-        print ('  '+ label['Type'])
-
-    return len(response['Labels'])
+    print('Detected texts in ' + photo)  
+    res = []  
+    for label in response['TextDetections']:
+        if (label['Confidence'] > 80):
+            print (label['Type'] + ' : ' + str(label['Confidence']))
+            print ('  '+ label['DetectedText'])
+            res.append(label['DetectedText'])
+    return res
 
 def main():
     photo='./images/img01.png'
 
-    label_count=detect_labels_local_file(photo)
-    print("Labels detected: " + str(label_count))
+    print(detect_labels_local_file(photo))
+    # print("Labels detected: " + str(label_count))
 
 
 if __name__ == "__main__":
